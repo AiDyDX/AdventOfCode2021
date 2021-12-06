0e32c525be5ea643160e1ac555b9e0c588cad3fb
AiDyDX <aidan@dydx.digital>
2021-12-06 15:44:47 +0200

    Day01 cleanup
    Cleaned up the functions for day01 into their own wrapper
    
    Moved the data to a new folder

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,24 +1,19 @@
 fun main() {
-    fun part1(input: List<Int>): Int {
-        return input.size
-    }
+    day01()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+}
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInputToInt("Day01_test")
-    check(part1(testInput) == 10)
-
+/**
+ * Functions implemented for day one
+ */
+fun day01() {
     // Gets depth increases and prints the amount
-    val input = readInputToInt("Day01")
+    val input = readInputToInt("./data/Day01")
     val depthIncrease: Int = countDepthIncreases(input)
     println("Depth increases: $depthIncrease")
 
     // Gets the averaged depth
-    val input2 = readInputToInt("Day01_Task2")
+    val input2 = readInputToInt("./data/Day01_Task2")
     val averagedDepthIncrease: Int = countAddedIncreases(input2)
     println("Added depth increases: $averagedDepthIncrease")
-
 }
