0923bec1a8cada644d9899e77641c3d168c6cb5a
AiDyDX <aidan@dydx.digital>
2021-12-06 13:14:50 +0200

    Day01 Part01 Completed
    Added  fun readInputToInt that converts a text file to a list of ints
    
    Added countDepthIncreases that takes a list of ints and counts how many times they increase.
    
    Updated main to call the functions and return the answer

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,5 +1,5 @@
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<Int>): Int {
         return input.size
     }
 
@@ -8,10 +8,12 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputToInt("Day01_test")
+    check(part1(testInput) == 10)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    // Gets depth increases and prints the amount
+    val input = readInputToInt("Day01")
+    val depthIncrease: Int = countDepthIncreases(input)
+    println(depthIncrease)
+
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,27 @@ import java.security.MessageDigest
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
 /**
+ * Reads lines from the given input txt file and converts them to integer.
+ */
+fun readInputToInt(name: String) = File("src", "$name.txt").readLines().map{ it.toInt()}
+
+/**
+ *
+ */
+fun countDepthIncreases(depths: List<Int>): Int {
+    val depthLength: Int = depths.size
+    var depthIncrease: Int = 0
+    var i = 1
+    while (i < depthLength){
+        if (depths[i-1] < depths[i]){
+            depthIncrease += 1
+        }
+        i += 1
+    }
+    return depthIncrease
+}
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
